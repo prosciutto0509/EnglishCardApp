9d44586fb50f4cd49fdb48cebe64a1f99e409ba6
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-08-24 14:17:24 +0900

    Initial Commit

---
+++ CardApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardApp
+//
+//  Created by 安田 悠麿 on 2020/08/24.
+//  Copyright © 2020 安田 悠麿. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
